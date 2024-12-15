cbd143158e7e79a453c0d1f5c95d4f08ccdb64b8
afnan <afnanballoor44@gmail.com>
2024-12-15 20:29:34 +0530

    added getUsersCount, getTableData, changePass

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,4 +1,7 @@
 const Admin = require('../models/adminModel')
+const User = require("../models/userModel")
+const Payment = require('../models/paymentModel')
+const QrCode = require('../models/qrCodeModel')
 const bcrypt = require("bcrypt")
 
 const verifyLogin = async(req,res) => {
@@ -26,4 +29,67 @@ const verifyLogin = async(req,res) => {
     }
 }
 
-module.exports = {verifyLogin}
+const getUsersCount = async (req,res) => {
+    try {
+        const usersCount = await User.countDocuments();
+
+        res.status(200).json({count: usersCount})
+    } catch (error) {
+        console.error("Error fetching user count",error)
+        res.status(500).json({ message: "Server error"})
+    }
+}
+
+const getTableData = async(req,res) => {
+    try {
+        const users = await User.find();
+
+        const tableData = await Promise.all(
+            users.map(async (user,index) => {
+                const payment = await Payment.findOne({kmc:user.kmc});
+                const qrCode = await QrCode.findOne({userName:user.name})                
+
+                return {
+                    id: index+1,
+                    name:user.name,
+                    place:user.place,
+                    kmcNo:user.kmc,
+                    phone:user.mobile,
+                    regTarrif:user.regTarrif,
+                    amount: payment ? payment.amount : "N/A",
+                    paymentId: payment ? payment.paymentId : "N/A",
+                    qrCode: qrCode ? qrCode.qrCodeImage : "N/A",
+                }
+            })
+        )
+        res.status(200).json(tableData)
+    } catch (error) {
+        console.error("Error fetching table data",error)
+        res.status(500).json({message: "Server error"})
+    }
+}
+
+const changePass = async(req,res) => {    
+    const {currentPassword , newPassword } = req.body;
+
+    try {
+        const admin = await Admin.findOne({username:'Admin'})
+        if(!admin){
+            return res.status(404).json({message:'Admin not found'})
+        }
+        const isMatch = await bcrypt.compare(currentPassword, admin.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Current password is incorrect' });
+        }
+
+        admin.password = await bcrypt.hash(newPassword, 10);
+        await admin.save()
+        
+        res.status(200).json({message:'Password updated successfully'})
+    } catch (error) {
+        res.status(500).json({message:'Server error',error})
+    }
+}
+
+
+module.exports = {verifyLogin, getUsersCount,getTableData,changePass}
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const { verifyLogin } = require('../controller/adminController')
+const { verifyLogin, getUsersCount, getTableData, changePass } = require('../controller/adminController')
 
 const router = express.Router()
 
 router.post('/adminLogin',verifyLogin)
+router.get('/userCount',getUsersCount)
+router.get('/table-data',getTableData)
+router.post('/change-password',changePass)
 
 module.exports = router;

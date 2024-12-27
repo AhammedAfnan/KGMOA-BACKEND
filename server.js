dfd6afff251eae67be85c888108165eafbc4c75b
afnan <afnanballoor44@gmail.com>
2024-12-27 13:46:11 +0530

    updated server.js

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: 'https://kgmoa.netlify.app/api', // Allow only this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-  };
+  origin: 'https://kgmoa.netlify.app', // Allow only this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  credentials: true, // Allow cookies and other credentials
+};
 
 // Middleware
 app.use(cors(corsOptions));

3da8a61f1a7bedb75f7058a68b44a5d6b1eb020a
afnan <afnanballoor44@gmail.com>
2024-12-30 17:31:07 +0530

    updated

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   place: { type: String, required: true },
-  kmc: { type: Number, required: true },
+  kmc: { type: String, required: true },
   mobile: { type: Number, required: true },
   regTarrif: {
     type: {
